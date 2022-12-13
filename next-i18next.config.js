47be814c0edc9d81cae1d42c9608a5aaa110208f
BuiKhacTien <buikhactien0203@gmail.com>
2022-12-13 18:05:07 +0700

    update next-i18next

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
 module.exports = {
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'sp', 'vn'],
+      localePath: path.resolve('./public/locales')
     },
     react: { useSuspense: false },
   }
--- pages/order-details/[id].jsx
+++ pages/order-details/[id].jsx
@@ -12,7 +12,7 @@ import OrderCardItem from "../../components/Account/OrderCard/OrderCardItem";
 //
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-export async function getStaticProps({ locale }) {
+export async function getServerSideProps({ locale }) {
    return {
       props: {
          ... (await serverSideTranslations(locale, ['translation'])),
